93537d2376c985ae476932533a05299f461ac2c9
MarcAlKareh <ma7748@student.st-chris.net>
2022-05-30 17:44:21 +0300

    Implemented eye recognition

--- main.py
+++ main.py
@@ -12,13 +12,19 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    if len(faces) != 0:
+    # if len(faces) != 0:
 
-        print("FACES")
+    #     print("FACES")
 
     for (x, y, w, h) in faces:
 
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 255, 255), 5)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 5)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.3, 5)
+
+        for (ex, ey, ew, eh) in eyes:
+          cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 5)
 
     cv2.imshow("Camera", frame)
     if cv2.waitKey(1) == ord('q'):
